96ae34b84470e8608b8e49efe67769beab56a8e0
RiyaKapuria <riyakapuria.1992@gmail.com>
2018-12-20 17:51:08 +0530

    Add Event Acknowledgements

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,20 @@
 var socket = io();
 
-socket.on('connect', function() {
-  console.log('Connected to Server!');
-})
+socket.on('connect', function () {
+  console.log('Connected to server');
+});
 
-socket.on('disconnect', function() {
-  console.log('Disconnected from Server!')
-})
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
+});
 
-socket.on('newMessage', function(message) {
-  console.log('newMessage', message)
-})
+socket.on('newMessage', function (message) {
+  console.log('newMessage', message);
+});
+
+socket.emit('createMessage', {
+  from: 'Riya',
+  text: 'Hi'
+}, function (data) {
+  console.log('Got it', data);
+});
--- server/server.js
+++ server/server.js
@@ -7,39 +7,36 @@ const socketIO = require('socket.io');
 
 const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 
-
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-  console.log('Connected to New user!');
+  console.log('New user connected');
 
   socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
-  
-  socket.on('createMessage', (message) => {
-    console.log("createMessage",message);
-    // io.emit('newMessage', {
+
+  socket.on('createMessage', (message, callback) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    callback('This is from the server.');
+    // socket.broadcast.emit('newMessage', {
     //   from: message.from,
     //   text: message.text,
     //   createdAt: new Date().getTime()
     // });
-    socket.broadcast.emit('newMessage', generateMessage(message.from, message.text));
   });
 
   socket.on('disconnect', () => {
-    console.log('User was Disconnected!')
-  })
+    console.log('User was disconnected');
+  });
 });
-
 
 server.listen(port, () => {
-  console.log(`Started on port ${port}!`);
+  console.log(`Server is up on ${port}`);
 });
-
-module.exports = {app};
