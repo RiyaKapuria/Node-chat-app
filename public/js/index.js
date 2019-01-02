c89a7878e0adf661806adac56be6da49df4d92dd
RiyaKapuria <riyakapuria.1992@gmail.com>
2019-01-02 13:10:00 +0530

    Add Message Form & jQuery

--- public/js/index.js
+++ public/js/index.js
@@ -1,20 +1,40 @@
 var socket = io();
 
-socket.on('connect', function () {
-  console.log('Connected to server');
+socket.on("connect", function() {
+  console.log("Connected to server");
 });
 
-socket.on('disconnect', function () {
-  console.log('Disconnected from server');
+socket.on("disconnect", function() {
+  console.log("Disconnected from server");
 });
 
-socket.on('newMessage', function (message) {
-  console.log('newMessage', message);
+socket.on("newMessage", function(message) {
+  console.log("newMessage", message);
+  var li = jQuery("<li></li>");
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery("#messages").append(li);
 });
 
-socket.emit('createMessage', {
-  from: 'Riya',
-  text: 'Hi'
-}, function (data) {
-  console.log('Got it', data);
+// socket.emit(
+//   "createMessage",
+//   {
+//     from: "Riya",
+//     text: "Hi"
+//   },
+//   data => {
+//     console.log("Got it", data);
+//   }
+// );
+
+jQuery("#message-form").on("submit", e => {
+  e.preventDefault();
+  socket.emit(
+    "createMessage",
+    {
+      from: "User",
+      text: jQuery("[name=message]").val()
+    },
+    () => {}
+  );
 });
